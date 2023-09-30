1a6430ec58ed6cee8928557de34c613d44695a41
volirra <127318805+volirra@users.noreply.github.com>
2023-09-30 18:11:30 +0300

    commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const pixabayAPI = {
+  baseUrl: 'https://pixabay.com/api/',
+  key: '39759324-a98081d45e1653f503d47aa2f',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  order: 'popular',
+  page: '1',
+  per_page: '40',
+};
