d7bca098d80c7ee65637479cba35c1a612d197ba
volirra <127318805+volirra@users.noreply.github.com>
2023-10-09 20:00:43 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -155,15 +155,15 @@ async function fetchImages(query, page, perPage) {
   return response.data;
 }
 
-SmoothScroll({
-  animationTime: 800,
-  stepSize: 75,
-  accelerationDelta: 30,
-  accelerationMax: 2,
-  keyboardSupport: true,
-  arrowScroll: 50,
-  pulseAlgorithm: true,
-  pulseScale: 4,
-  pulseNormalize: 1,
-  touchpadSupport: true,
-});
+//SmoothScroll({
+  //animationTime: 800,
+  //stepSize: 75,
+  //accelerationDelta: 30,
+  //accelerationMax: 2,
+  //keyboardSupport: true,
+  //arrowScroll: 50,
+  //pulseAlgorithm: true,
+  //pulseScale: 4,
+  //pulseNormalize: 1,
+  //touchpadSupport: true,
+//});
