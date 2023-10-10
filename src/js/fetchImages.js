fc007145f00613cccb72bb685ccce1254e90004f
volirra <127318805+volirra@users.noreply.github.com>
2023-10-10 20:37:06 +0300

    commit

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -4,15 +4,6 @@ const KEY = '39759324-a98081d45e1653f503d47aa2f';
 
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-axios.interceptors.response.use(
-  response => {
-    return response;
-  },
-  error => {
-    Notiflix.Notify.failure('Something went wrong. Please try again later.');
-    return Promise.reject(error);
-  }
-);
 
 export async function fetchImages(query, page, perPage=40) {
   const response = await axios.get(
